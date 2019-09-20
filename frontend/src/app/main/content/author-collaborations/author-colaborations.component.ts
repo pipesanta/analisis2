4046641cb1c03c2c34788c03d971c899bcb82a24
pipesanta <juansantag95@gmail.com>
2019-09-20 09:32:32 -0500

    clase del viernes

--- frontend/src/app/main/content/author-collaborations/author-colaborations.component.ts
+++ frontend/src/app/main/content/author-collaborations/author-colaborations.component.ts
@@ -5,17 +5,19 @@ import { filter, tap, mergeMap, debounceTime } from 'rxjs/operators';
 import { AuthorColaborationsService } from './author-colaborations.service';
 
 @Component({
-// tslint:disable-next-line: component-selector
+  // tslint:disable-next-line: component-selector
   selector: 'author-colaborations',
   templateUrl: './author-colaborations.component.html',
   styleUrls: ['./author-colaborations.component.scss']
 })
 export class AuthorColaborationsComponent implements OnInit, OnDestroy {
+
   filterTextControl = new FormControl('');
+  resultList = [];
 
   constructor(
     private authorCollaborationsService: AuthorColaborationsService
-    ){
+  ) {
 
   }
 
@@ -24,20 +26,35 @@ export class AuthorColaborationsComponent implements OnInit, OnDestroy {
   }
 
 
-  listenSearchbar(){
+  listenSearchbar() {
     this.filterTextControl.valueChanges
-    .pipe(
-      filter(filterText => filterText != null),
-      debounceTime(250),
-      tap(filterText => console.log('Buscar por el author que conicida con  ==> ', filterText)),
-      mergeMap(filterText => this.authorCollaborationsService.getArticlesByAuthor$(filterText))
-    ).subscribe(results =>  this.showResults(results) )
+      .pipe(
+        filter((filterText: any) => {
+          return filterText != null && filterText !== ''
+        }),
+        debounceTime(500),
+        tap(filterText => console.log('Buscar por el author que conicida con  ==> ', filterText)),
+        mergeMap(filterText => this.authorCollaborationsService.getArticlesByAuthor$(filterText))
+      )
+      .subscribe(results => {
+        this.resultList = results;
+      }
+
+
+      );
   }
 
-  showResults(results: any[]): void{
-    console.log('Mostrar los sisguientes resultados ==> ', results)
+  showResults(results: any[]): void {
+    console.log('Mostrar los sisguientes resultados ==> ', results);
+
+
+
   }
 
   ngOnDestroy() {
+
+
+
+
   }
 }
--- frontend/src/app/main/content/author-collaborations/author-colaborations.service.ts
+++ frontend/src/app/main/content/author-collaborations/author-colaborations.service.ts
@@ -4,11 +4,15 @@ import  { of, from } from 'rxjs';
 import { GatewayService } from '../../../api/gateway.service';
 import { PART_O1 } from '../../fake-db/part01';
 import { filter, toArray } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 
 @Injectable()
 export class AuthorColaborationsService {
-  constructor(private gateway: GatewayService) {}
+  constructor(
+    private gateway: GatewayService,
+    private httpClient: HttpClient
+    ) {}
 
   getArticlesByAuthor$(filterText: string){
     return from(PART_O1)
@@ -16,6 +20,8 @@ export class AuthorColaborationsService {
       // filter(article => (article.name.toUpperCase()).includes(filterText.toUpperCase()))
       toArray()
     );
+
+    // return this.httpClient.get("localhost:getAuthorCol")
 
   }
 
