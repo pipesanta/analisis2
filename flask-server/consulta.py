6ff42af5bfc6a66986080f202b51fa38eec97dc5
danielamgx <ldmg159@gmail.com>
2019-10-12 13:08:44 -0500

    Add Encoding

--- flask-server/consulta.py
+++ flask-server/consulta.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
 #path="https://github.com/pipesanta/analis2/blob/master/university_of_antioquia.json?raw=true" ##JSON GRANDE del repositorio de otro grupo.
 path="https://github.com/stevenstiven2000/activity-categorization-implementation/blob/master/udea.json?raw=true" ##JSON del repositorio de Stiven
