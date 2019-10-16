e68d142b343ddf49db7222d6cc08ee4fb9983932
David Mejia <dmrmejia57@gmail.com>
2019-10-15 22:07:25 -0500

    Updates frontend JournalCharacterization

--- frontend/src/app/main/content/journals-characterisation/journals-characterisation.service.ts
+++ frontend/src/app/main/content/journals-characterisation/journals-characterisation.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import  { of, from } from 'rxjs';
+import { of, from } from 'rxjs';
 import { PART_O1 } from '../../fake-db/part01';
 import { filter, toArray } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
+import { GUTI_01, GUTI_02 } from '../../fake-db/part01';
 
+
+const SERVER_POST = 'http://localhost:7172/';
 
 @Injectable()
 export class JournalsCharacterisationService {
@@ -12,16 +15,13 @@ export class JournalsCharacterisationService {
     private httpClient: HttpClient
     ) {}
 
-  getAnyInfoToTest$(filterText: string){
-    return from(PART_O1)
-    .pipe(
-      // filter(article => (article.name.toUpperCase()).includes(filterText.toUpperCase()))
-      toArray()
-    );
-
-    // return this.httpClient.get("localhost:getAuthorCol")
+    getAnyInfoToTest$(filterText: string){
+      return of(PART_O1);
+      // return this.httpClient.get(`${ip}institution/${filterText}`);
 
   }
+  searchJournals$(textFilter: string) {
+    return this.httpClient.get(`${SERVER_POST}journalCharacterisation/${textFilter}`);
+  }
 
-  
 }
